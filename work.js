69b6019dfed712cd413b7773f9f8a74a8c4f58d9
Prashant Kumar <prashantkumardgdg@gmail.com>
2025-11-23 23:41:20 +0530

    update

--- work.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("searchBtn").addEventListener("click", function () {
-    let text = document.getElementById("searchInput").value;
-
-    if (text === "") {
-        alert("Please type something to search.");
-    } else {
-        alert("You searched for: " + text);
-    }
-});
